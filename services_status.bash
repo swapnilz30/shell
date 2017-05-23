b37179f42aca46a5931401ce4fb1997f2833c594
Swapnil <swapnilz30@users.noreply.github.com>
2017-05-23 12:47:30 +0530

    Script for centos 7
    This script checks the declared services are working or not.

---
+++ services_status.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+#service_status=`sudo systemctl  status nginx  | grep Active | cut -d" " -f5 | cut -d" " -f1`
+services=(redis mongod elasticsearch php-fpm nginx uwsgi celeryd celerybeatd postgresql-9.4 tomcat cyops-auth)
+
+
+for service in ${services[@]}; do
+        service_status=`sudo systemctl  status $service  | grep Active | cut -d" " -f5`
+	if [ "active" ==  $service_status ]
+	then
+	       echo ${green}$service " is active.${reset}"
+	else
+	       echo ${red}$service " is inactive.${reset}"
+	fi
+done
+#service_status=`sudo systemctl  status redis  | grep Active | cut -d" " -f5`
+#
+#echo $service_status
+#
